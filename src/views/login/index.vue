9610ead4a559ed26698ecc746c03afebad85abf6
dongjia <dongjia@bkrwin.com>
2022-09-21 20:19:26 +0800

    first commit

---
+++ src/views/login/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <el-row>
+        <el-button type="primary" @click="handleLogin">登录</el-button>
+    </el-row>
+</template>
+
+<script>
+export default {
+    name:'Login',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        handleLogin(){
+            this.$router.push('/registration');
+        }
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/registration/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row>
+      <el-button type="success">注册</el-button>
+  </el-row>
+</template>
+
+<script>
+export default {
+    name: 'Registration',
+    data() {
+        return {
+          
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
